73edd09ee783372b1d2abef2136a9b3adff95e92
Lucas Fonte <lucasfonte@ufl.edu>
2024-11-18 23:35:52 -0500

    done

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 def main():
     try:
@@ -9,13 +9,29 @@ def main():
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        x = random.randrange(0, 20)
+        y = random.randrange(0, 16)
+
         running = True
         while running:
+            screen.fill("light green")
+            for i in range(20):
+                pygame.draw.line(screen, "dark blue", (i * 32, 0), (i * 32, 512))
+            for i in range(16):
+                pygame.draw.line(screen, "dark blue", (0, i * 32), (640, i * 32))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(32 * x, 32 * y)))
+            pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
-            pygame.display.flip()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = event.pos
+                    if x * 32 <= mouse_x < (x + 1) * 32 and y * 32 <= mouse_y < (y + 1) * 32:
+                        x = random.randrange(0, 20)
+                        y = random.randrange(0, 16)
+                        screen.blit(mole_image, mole_image.get_rect(topleft=(32 * x, 32 * y)))
+
             clock.tick(60)
     finally:
         pygame.quit()
